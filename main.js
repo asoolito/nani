c492fb81ae85bae735d57fb2c46deb9f41e62e5e
asoolito <roblox890king890@gmail.com>
2024-05-27 12:40:26 +0500

    hello

---
+++ main.js
@@ -0,0 +1,12 @@
+let userName = ["olma", "Banan", "Gilos", "Olcha"]
+console.log(userName);
+let userMalumot = alert(`Boshlanish arrayning uzunligi ${userName.length}`);
+let userSorov = confirm("Siz arrayingizdan malumotlarni olib tashlamoqchimisiz")
+let userNew = alert("Arraynig uzunligi 3")
+if (userSorov == true) {
+    console.log(userName.pop())
+    console.log(userName)
+} else {
+    console.log(userName);
+}
+console.log(userName);
