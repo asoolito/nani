840503dbcd1207d1db99ce0b909d997fa8b4d337
asoolito <roblox890king890@gmail.com>
2024-05-27 12:54:37 +0500

    hj

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ let userName = ["olma", "Banan", "Gilos", "Olcha"]
 console.log(userName);
 let userMalumot = alert(`Boshlanish arrayning uzunligi ${userName.length}`);
 let userSorov = confirm("Siz arrayingizdan malumotlarni olib tashlamoqchimisiz")
-let userNew = alert("Arraynig uzunligi 3")
+let userNew 
+ m= alert("Arraynig uzunligi 3")
 if (userSorov == true) {
     console.log(userName.pop())
     console.log(userName)
